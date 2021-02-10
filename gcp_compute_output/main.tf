fdadffdd476611040120e3382b742d23bf62911e
Giang Nguyen <ginguyen@paloaltonetworks.com>
2021-02-09 21:07:05 -0800

    Modified files:[main.tf]

--- gcp_compute_output/main.tf
+++ gcp_compute_output/main.tf
@@ -37,3 +37,5 @@ resource "google_compute_instance" "default" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2021-02-09 21:07:05.162322]
